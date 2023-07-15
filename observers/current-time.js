b964e3ab09875e4a3d3ecd1558215bd478c3ebdf
cdrani <charlesdrani@gmail.com>
2023-07-14 18:22:16 -0600

    Rewrite CurrentTimeObserver without MutationObserver

--- observers/current-time.js
+++ observers/current-time.js
@@ -1,45 +1,108 @@
 class CurrentTimeObserver {
-    constructor({ video, snip }) {
-        this._snip = snip
-        this._video = video
-        this._observer = null
+    #snip
+    #video
+    #observer
 
+    constructor({ video, snip }) {
+        this.#snip = snip
+        this.#video = video
+
+        this.#init()
         this.observe()
     }
 
+    #init() {
+        const { isSnip } = this.#songState
+        if (!isSnip) return
+
+        const currentTime = timeToSeconds(this.#playbackPosition?.textContent || '0:00')
+
+        if (currentTime > 0) {
+            this.#video.currentTime = currentTime
+        }
+    }
+
+    get #playbackPosition() {
+        return document.querySelector('[data-testid="playback-position"]')
+    }
+
+    get #nextButton() {
+        return document.querySelector('[data-testid="control-button-skip-forward"]')
+    }
+
+    get #muteButton() {
+        return document.querySelector('[data-testid="volume-bar-toggle-mute-button"]')
+    }
+
+    get #muted() {
+        return this.#muteButton.ariaLabel == 'Unmute'
+    }
+
+    get #songState() {
+        return this.#snip.read()
+    }
+
+    get #isSkippable() {
+        const { isSkipped, endTime } = this.#songState
+        return isSkipped || endTime == 0
+    }
+
+    get #isLooping() {
+        const repeatButton = document.querySelector('[data-testid="control-button-repeat"]') 
+        return repeatButton?.ariaLabel === 'Disable repeat'
+    }
+ 
+    get #atEnd() {
+        const { isSnip, endTime } = this.#songState
+        return Boolean(isSnip) && endTime == parseInt(this.#video.currentTime)
+    }
+
+    get #atSnip()  {
+        const { isSnip, startTime } = this.#songState
+        if (!isSnip) return false
+
+        return isSnip && parseInt(startTime) >= this.#video.currentTime 
+    }
+
+    #handleSkippable() {
+        this.#video.currentTime = 0
+        this.#nextButton.click()
+    }
+
     observe() {
-        const target = document.querySelector('[data-testid="playback-position"]')
-        this._observer = new MutationObserver(this._handler)
-        this._observer.observe(target, { childList: true, subtree: true, characterData: true })
-    }
+        this.#observer = setInterval(() => {
+            const { isSkipped, startTime, endTime } = this.#songState
+            const currentDisplayTime = secondsToTime(parseInt(this.#video.currentTime, 10))
 
-    disconnect() {
-        this._observer?.disconnect()
-    }
+            this.#playbackPosition.textContent = currentDisplayTime
+            
+            if (isSkipped && !this.#muted) {
+                this.#muteButton.click()
+            }
 
-    _handler = mutationsList => {
-        for (const mutation of mutationsList) {
-            if (mutation.type === 'characterData' && mutation.target.data) {
-                const targetTime = mutation.target.data
+            if (this.#isSkippable) {
+                this.#handleSkippable()
+            } else {
+                if (this.#muted) {
+                    this.#muteButton.click()
+                }
 
-                const { startTime, endTime, isSnip } = this._snip.read()
-                const repeatButton = document.querySelector('[data-testid="control-button-repeat"]')
-                const isLooping = repeatButton?.ariaLabel === 'Disable repeat'
-                const nextButton = document.querySelector(
-                    '[data-testid="control-button-skip-forward"]'
-                )
-
-                const atEnd = Boolean(isSnip) && endTime && secondsToTime(endTime) === targetTime
-                const loopAtEnd = atEnd && isLooping
-
-                if (atEnd) {
-                    if (loopAtEnd) {
-                        this._video.currentTime = startTime
-                    } else {
-                        nextButton.click()
+                if (this.#atSnip) {
+                    this.#video.currentTime = startTime
+                } else if (this.#atEnd && this.#isLooping) {
+                    this.#video.currentTime = startTime + 0.1
+                } else if (this.#video.currentTime >= endTime) {
+                    this.#nextButton.click()
+                    if (!this.#muted) {
+                        this.#muteButton.click()
                     }
                 }
             }
-        }
+        }, 1000)
+    }
+
+    disconnect() {
+        clearInterval(this.#observer)
+        this.#observer = null
     }
 }
