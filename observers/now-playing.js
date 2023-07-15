545af6859b4e2208e519edbe0c2efe58c175ba61
cdrani <charlesdrani@gmail.com>
2023-07-14 18:36:46 -0600

    Rewrite NowPlayingObserver to use private features

--- observers/now-playing.js
+++ observers/now-playing.js
@@ -1,8 +1,9 @@
 class NowPlayingObserver {
-    constructor(snip) {
-        this._snip = snip
-        this._observer = null
+    #snip
+    #observer
 
+    constructor(snip) {
+        this.#snip = snip
         this.observe()
     }
 
@@ -10,27 +11,27 @@ class NowPlayingObserver {
         const target = document.querySelector('[data-testid="now-playing-widget"]')
         const config = { subtree: true, childList: true, attributes: true }
 
-        this._observer = new MutationObserver(this._handler)
-        this._observer.observe(target, config)
+        this.#observer = new MutationObserver(this.#handler)
+        this.#observer.observe(target, config)
     }
 
-    _isAnchor(mutation) {
+    #isAnchor(mutation) {
         return (
             mutation.type === 'attributes' &&
-            mutation.target.nodeName == 'A' &&
+            mutation.target.localName == 'a' &&
             mutation.attributeName === 'href'
         )
     }
 
-    _handler = mutationsList => {
+    #handler = mutationsList => {
         for (const mutation of mutationsList) {
-            if (this._isAnchor(mutation)) {
-                this._snip.updateView()
+            if (this.#isAnchor(mutation)) {
+                this.#snip.updateView()
             }
         }
     }
 
     disconnect() {
-        this._observer?.disconnect()
+        this.#observer?.disconnect()
     }
 }
