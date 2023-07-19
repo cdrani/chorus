39ea46a126451a2b1610372c8ca10a9af45309d2
Geraldo PMJ <geraldo.pmj@gmail.com>
2023-07-19 08:05:38 -0300

    Update header.js to svg

--- components/header.js
+++ components/header.js
@@ -1,14 +1,17 @@
 const createHeader = () => `
     <div class="chorus-common">
         <span class="chorus-header">chorus</span>
-        <div style="display: flex; justify-content: flex-end;">
-            <div style="justify-content: space-between">
-                <button id="chorus-close-button" class="chorus-text-button">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-        </div>
+        <button id="chorus-close-button" class="chorus-text-button">
+            <svg 
+                role="img"
+                fill="none"
+                width="20px"
+                height="20px"
+                stroke="#fff"
+                stroke-width="2"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
     </div>
-`
