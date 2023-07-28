f7832db45f8c59c86e94db1b2d7b783b2b05610f
cdrani <charlesdrani@gmail.com>
2023-07-27 23:29:06 -0600

    Create copyToClipBoard util function

---
+++ utils/clipboard.js
@@ -0,0 +1,8 @@
+export const copyToClipBoard = (text) => {
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            console.log(`Copied ${text} to clipboard`)
+        }).catch(err => {
+            console.error(`Error copying text to clipboard: ${err}`)
+        })
+}
