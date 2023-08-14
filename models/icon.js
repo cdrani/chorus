897afa7e27bd36908ac8bd2cef0ab4899be0f7b3
cdrani <charlesdrani@gmail.com>
2023-08-14 00:22:06 -0600

    fix: replace div with button for a11y

--- models/icon.js
+++ models/icon.js
@@ -11,18 +11,23 @@ export default class Icon {
 
     #createIcon() {
         return `
-            <div id="chorus-icon" class="chorus-hover-white" role="snip" aria-label="Edit Snip">
+            <button 
+                role="snip"
+                id="chorus-icon"
+                aria-label="Edit Snip"
+                class="chorus-hover-white"
+                style="padding:0 6px;border:none;background:none;display:flex;justify-content:center;align-items:center;"
+            >
                 <svg
-                    xmlns="http://www.w3.org/2000/svg"
                     fill="none"
+                    width="1.25rem"
+                    height="1.25rem"
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    width="2rem"
-                    height="2rem"
-                    style="padding: 0.375rem;"
-                    preserveAspectRatio="xMidYMid meet"
                     id="chorus-highlight"
+                    preserveAspectRatio="xMidYMid meet"
+                    xmlns="http://www.w3.org/2000/svg"
                 >
                     <path
                         stroke-linecap="round"
@@ -30,7 +35,7 @@ export default class Icon {
                         d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
                     />
                 </svg>
-            </div>
+            </button>
         `
     }
 }
