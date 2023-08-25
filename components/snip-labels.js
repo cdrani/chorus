9a75fb4692a0f6fae7711637a73a2f6ee1a146ac
cdrani <charlesdrani@gmail.com>
2023-08-25 12:48:11 -0600

    refactor: rename labels to snip-labels

--- components/snip-labels.js
+++ components/snip-labels.js
@@ -1,8 +1,8 @@
 const style = "min-width:24px;font-size:0.85rem;height:100%;text-align:end;margin-left:4px"
 
 export const createRangeLabels = () => `
-    <div style="display: flex; justify-content: space-between">
-        <div style="display: flex; flex-wrap: wrap; max-width: 100px;">
+    <div style="display:flex;justify-content:space-between;height:3rem;align-items:center;">
+        <div style="display:flex;flex-wrap:wrap;height:1.5rem;max-width:100px;">
             <label class="chorus-common" style="width: 100%">
                 start:
                 <span
