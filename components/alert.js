23daad761cc5e4669c429f3b2dc822004314ffba
cdrani <charlesdrani@gmail.com>
2023-08-28 14:47:08 -0600

    refactor: move alert style to styles.css

--- components/alert.js
+++ components/alert.js
@@ -1,10 +1,11 @@
 export const createAlert = () => `
-    <div 
-        id="chorus-alert"
-        style="display:none;justify-content:space-between;align-items:center;border-radius:.5rem;z-index:99999;padding:.5rem 1.25rem;padding-right:.75rem;min-width:320px;width:60%;background:#1ed670;position:absolute;bottom:10%;right:2.5%;"
-    >
+    <div id="chorus-alert">
         <p id="chorus-alert-message" style="font-size:1.25rem;color:#fff;"></p>
-        <button id="chorus-alert-close-button" class="chorus-text-button" style="padding:.5rem;align-self:center;background:none;border:none">
+        <button 
+            id="chorus-alert-close-button"
+            class="chorus-text-button"
+            style="padding:.5rem;align-self:center;background:none;border:none"
+        >
             <svg 
                 role="img"
                 fill="none"
