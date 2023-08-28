94ca49a412527f3e0a3b50ec1919d93b8abf241b
cdrani <charlesdrani@gmail.com>
2023-08-28 11:30:13 -0600

    refactor: remove snip-controls.js

--- models/snip/snip.js
+++ models/snip/snip.js
@@ -1,15 +1,16 @@
-import SnipControls from './snip-controls.js'
 import ButtonListeners from '../../events/listeners.js'
+import SliderControls from '../slider/slider-controls.js'
 
 export default class Snip {
     constructor(store) {
         this._store = store
-        this._controls = new SnipControls()
+        this._controls = new SliderControls()
         this._listeners = new ButtonListeners(this)
     }
 
     init() {
         this._listeners.init()
+        this._controls.init()
         this.isEditing = true
     }
 
