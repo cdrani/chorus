623b006f97094af28022600f4afda1df56590680
cdrani <charlesdrani@gmail.com>
2023-09-20 13:30:19 -0600

    refactor: split listener into action & header classes

---
+++ src/events/listeners/listeners.js
@@ -0,0 +1,18 @@
+import Seek from '../../models/seek/seek.js'
+import Speed from '../../models/speed/speed.js'
+import CurrentSnip from '../../models/snip/current-snip.js'
+
+export default class Listeners {
+    constructor() {
+        this._seek = new Seek()
+        this._speed = new Speed()
+        this._snip = new CurrentSnip()
+    }
+
+    _hide() {
+        this._snip.isEditing = false
+        const mainElement = document.getElementById('chorus-main')
+
+        mainElement.style.display = 'none'
+    }
+}
