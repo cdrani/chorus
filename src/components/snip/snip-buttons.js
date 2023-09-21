cea0ecff4c06b4b1d404153487b8793c9d9f7de1
cdrani <charlesdrani@gmail.com>
2023-09-21 10:28:18 -0600

    refactor: snip-buttons component

--- src/components/snip/snip-buttons.js
+++ src/components/snip/snip-buttons.js
@@ -1,28 +1,20 @@
+const createButton = ({ text, style }) => `
+    <button
+        class="chorus-text-button"
+        id="chorus-snip-${text}-button"
+        style="padding:0 10px;height:100%;font-size:1rem;${style || ''}"
+    >
+        <span>${text}</span>
+    </button>
+`
+
 export const createSnipButtons = () => `
     <div style="display:flex;margin-top:12px;justify-content:space-between;height:1.5rem;">
-        <button
-            class="chorus-text-button"
-            id="chorus-snip-share-button"
-            style="display:flex;padding:0 10px;height:100%;font-size:1rem;"
-        >
-            <span>share</span>
-        </button>
+        ${createButton({ text: 'share', style: 'display:flex;padding:0 10px' })}
         <div style="display:flex;justify-content:flex-end">
             <div style="display:flex;align-items:center;justify-content:space-between">
-                <button
-                    class="chorus-text-button"
-                    id="chorus-snip-remove-button"
-                    style="padding:0 10px;height:100%;margin-right:0.5rem;font-size:1rem;"
-                >
-                    <span>remove</span>
-                </button>
-                <button
-                    id="chorus-snip-save-button"
-                    class="chorus-text-button"
-                    style="font-size:1rem;padding:0 10px;height:100%;"
-                >
-                    <span>save</span>
-                </button>
+                ${createButton({ text: 'remove', style: 'margin-right:0.5rem;display:flex' })}
+                ${createButton({ text: 'save' })}
             </div>
         </div>
     </div>
