330b9f2f971690b4a1f25ad876d788708bc87635
cdrani <charlesdrani@gmail.com>
2023-09-21 10:37:26 -0600

    refactor: snip-labels component

--- src/components/snip/snip-labels.js
+++ src/components/snip/snip-labels.js
@@ -1,25 +1,16 @@
 const style = "min-width:24px;font-size:0.85rem;height:100%;text-align:end;margin-left:4px"
 
+const createLabel = text => `
+    <div>
+        <label class="chorus-common" style="width:100%">
+            ${text}: <span style="${style}" id="chorus-${text}"></span>
+        </label>
+    </div>
+`
+
 export const createRangeLabels = () => `
     <div style="display:flex;justify-content:space-between;height:3rem;align-items:center;">
-        <div style="display:flex;flex-wrap:wrap;height:1.5rem;max-width:100px;">
-            <label class="chorus-common" style="width: 100%">
-                start:
-                <span
-                    id="chorus-start"
-                    style="${style}"
-                ></span>
-            </label>
-        </div>
-
-        <div style="display: flex; flex-wrap: wrap; max-width: 100px;">
-            <label class="chorus-common" style="width: 100%">
-                end:
-                <span
-                    id="chorus-end"
-                    style="${style}"
-                ></span>
-            </label>
-        </div>
+        ${createLabel('start')}
+        ${createLabel('end')}
     </div>
 `
