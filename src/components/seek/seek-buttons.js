5efe920221e19fe45a3dabf2d555ed0958fd97a9
cdrani <charlesdrani@gmail.com>
2023-09-22 10:37:33 -0600

    refactor: seek and speed buttons components

--- src/components/seek/seek-buttons.js
+++ src/components/seek/seek-buttons.js
@@ -1,19 +1,8 @@
+import { createTextButton } from '../text-button.js'
+
 export const createSeekButtons = () => ` 
     <div style="display:flex;margin-top:12px;justify-content:space-between;height:1.5rem;">
-        <button
-            class="chorus-text-button"
-            id="chorus-seek-reset-button"
-            style="padding:0 10px;height:100%;font-size:1rem;"
-        >
-            <span>reset</span>
-        </button>
-
-        <button
-            id="chorus-seek-save-button"
-            class="chorus-text-button"
-            style="font-size:1rem;padding:0 10px;height:100%;"
-        >
-            <span>save</span>
-        </button>
+        ${createTextButton({ id: 'seek-reset', text: 'reset' })}
+        ${createTextButton({ id: 'seek-save', text: 'save' })}
     </div>
 `
