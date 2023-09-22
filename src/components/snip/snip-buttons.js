c6fd874fef45a70ee6ec4618b12881473c90b572
cdrani <charlesdrani@gmail.com>
2023-09-22 11:11:40 -0600

    refactor: snip-buttons component

--- src/components/snip/snip-buttons.js
+++ src/components/snip/snip-buttons.js
@@ -1,20 +1,16 @@
-const createButton = ({ text, style }) => `
-    <button
-        class="chorus-text-button"
-        id="chorus-snip-${text}-button"
-        style="padding:0 10px;height:100%;font-size:1rem;${style || ''}"
-    >
-        <span>${text}</span>
-    </button>
-`
+import { createTextButton } from '../text-button.js'
 
 export const createSnipButtons = () => `
     <div style="display:flex;margin-top:12px;justify-content:space-between;height:1.5rem;">
-        ${createButton({ text: 'share', style: 'display:flex;padding:0 10px' })}
+        ${createTextButton({ id: 'snip-share', text: 'share', style: 'display:flex;padding:0 10px' })}
         <div style="display:flex;justify-content:flex-end">
             <div style="display:flex;align-items:center;justify-content:space-between">
-                ${createButton({ text: 'remove', style: 'margin-right:0.5rem;display:flex' })}
-                ${createButton({ text: 'save' })}
+                ${createTextButton({ 
+                      text: 'remove', 
+                      id: 'snip-remove',
+                      style: 'margin-right:0.5rem;display:flex'
+                })}
+                ${createTextButton({ id: 'snip-save', text: 'save' })}
             </div>
         </div>
     </div>
