2652949ee0cf20459b91e14db32fc9c943f94fcf
cdrani <charlesdrani@gmail.com>
2023-09-28 23:03:59 -0600

    refactor: highlight util

--- src/utils/higlight.js
+++ src/utils/higlight.js
@@ -1,8 +1,18 @@
-const isHighlightable = ({ isSnip, preservesPitch = true, playbackRate = '1' }) => (
-    isSnip || !preservesPitch || playbackRate !== '1'
+const isHighlightable = ({ 
+    isSnip, 
+    isShared, 
+    playbackRate = '1',
+    preservesPitch = true,
+}) => (
+    (isSnip && !isShared) || !preservesPitch || playbackRate !== '1'
 )
 
-export const highlightElement = ({ selector, songStateData, context = document, property = 'stroke' }) => {
+export const highlightElement = ({ 
+    selector, 
+    songStateData,
+    context = document,
+    property = 'stroke', 
+}) => {
     const element = context.querySelector(selector)
     const fillColor = isHighlightable(songStateData) ? '#1ed760' : 'currentColor'
 
