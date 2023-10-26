0398f134e0b7a109f68a85613198e5e17d7107cc
cdrani <charlesdrani@gmail.com>
2023-10-25 19:30:09 -0600

    fix: set img crossOrigin attribute to Anonymous

--- src/popup/index.js
+++ src/popup/index.js
@@ -52,6 +52,7 @@ async function loadImage({ url, elem }) {
     return new Promise((resolve, reject) => {
         elem.onload = () => resolve(elem)
         elem.onerror = reject
+        elem.crossOrigin = 'Anonymous'
         elem.src = url
     })
 }
