e7a8349120f53cc89954c9456397962ff08b8300
cdrani <charlesdrani@gmail.com>
2023-11-02 17:00:22 -0600

    refactor: update Alert#displayAlert method

--- src/models/alert.js
+++ src/models/alert.js
@@ -2,15 +2,17 @@ import { createAlert } from '../components/alert.js'
 import { parseNodeString } from '../utils/parser.js'
 
 export default class Alert {
-    displayAlert() {
+    displayAlert({ type = 'success', duration = 3000, message } = {}) {
         this.#setupAlert()
         const alertMessage = this.#chorusAlert.querySelector('[id="chorus-alert-message"]')
+        this.#chorusAlert.style.backgroundColor = type == 'success' ? 'green' : 'red'
 
-        alertMessage.textContent = `Snip copied to clipboard.`
+        
+        alertMessage.textContent = message ?? 'Snip copied to clipboard.'
         this.#chorusAlert.style.display = 'flex' 
         setTimeout(() => { 
             this.#chorusAlert.style.display = 'none' 
-        }, 3000)
+        }, duration)
     }
 
     #handleAlert(target) {
