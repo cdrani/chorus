6c68a78be703fa3db4323aeddc22e50b1c64ef7a
cdrani <charlesdrani@gmail.com>
2023-11-09 19:28:01 -0700

    style: update effects ui text + container styles

--- src/components/effects/effects-controls.js
+++ src/components/effects/effects-controls.js
@@ -4,10 +4,16 @@ import { createEffectsSelector } from './effects-selector.js'
 import { drinkPresetNames } from '../../lib/reverb/presets.js'
 export const createEffectsControls = () => `
     <div id="chorus-effects-controls" style="display: none">
-        <div style="display:flex;flex-direction:column;justify-content:space-between;">
+        <div style="display:flex;flex-direction:column;justify-content:space-evenly;height:6rem;">
+            <div style="font-size:.75rem;color:#fafafa">
+                <p>
+                    Presets For Reverb Effects. Pair With Speed & Pitch 
+                    Correction Combos for New Aural Experiences.
+                </p> 
+            </div>
             ${createEffectsSelector({
                 name: 'drink-effect',
-                labelName: '"Drink" Reverb',
+                labelName: 'Drink Size',
                 optionNames: drinkPresetNames
             })}
         </div>
