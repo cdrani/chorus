e23fe938adc8ca03f6c94dc437c5c4bb7220d07f
cdrani <charlesdrani@gmail.com>
2023-11-20 22:36:48 -0700

    refactor: utils song

--- src/utils/song.js
+++ src/utils/song.js
@@ -14,16 +14,10 @@ export const currentSongInfo = () => {
     const contextTypeIdFromHead = document.querySelector('link[rel="canonical"]')
         ?.href?.split(`${contextType ?? 'track'}/`)?.at(-1)
 
-    const trackId = contextTypeId ?? contextTypeIdFromHead
+    const track_id = contextTypeId ?? contextTypeIdFromHead
     const type = contextType ?? 'track'
-    return  {
-        id,
-        type,
-        cover: image?.src,
-        track_id: trackId,
-        url: `${location.origin}/${type}/${trackId}`,
-    }
-
+    const url = `${location.origin}/${type}/${track_id}`
+    return  { id, type, cover: image?.src, track_id, url }
 }
 
 export const trackSongInfo = row => {
@@ -53,16 +47,15 @@ const getTrackId = row => {
     if (!trackIdUrl) return {}
 
     const url = trackIdUrl.split('.com').at(1)
-    return {
-        url,
-        track_id: url.split('/').at(2)
-    }
+    return { url, track_id: url.split('/').at(2) }
 }
 
 const getArtists = row => {
     const artistsList = row.querySelectorAll('span > div > a')
     // Here means we are at artist page and can get name from h1
-    if (!artistsList.length) return document.querySelector('span[data-testid="entityTitle"] > h1').textContent
+    if (!artistsList?.length) { 
+        return document.querySelector('span[data-testid="entityTitle"] > h1')?.textContent || ''
+    }
 
     return Array.from(artistsList).filter(artist => artist.href.includes('artist')).map(artist => artist.textContent).join(', ')
 }
