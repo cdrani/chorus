8ed4684ad81ce1abd12e4a9c4117e4e107f593ce
cdrani <charlesdrani@gmail.com>
2023-11-30 13:44:13 -0700

    refactor: use api functions in player service

--- src/services/player.js
+++ src/services/player.js
@@ -1,35 +1,25 @@
 import { getState } from '../utils/state.js'
-import { setOptions, request } from '../utils/request.js'
-
-const API_URL = 'https://api.spotify.com/v1/me/player'
+import { makeRequest, handleRequest } from './api.js'
 
 function playSharedTrack({ track_id, position }) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const body = {
-                uris: [`spotify:track:${track_id}`],
-                position_ms: Math.max(parseInt(position, 10), 0) * 1000,
-            }
-            const options = await setOptions({ method: 'PUT', body })
-            const device_id = await getState('device_id')
-            const url = `${API_URL}/play?device_id=${device_id}`
-            
-            const response = await request({ url, options })
-            resolve(response)
-        } catch(error) { reject(error) }
+    return handleRequest(async () => {
+        const body = {
+            uris: [`spotify:track:${track_id}`],
+            position_ms: Math.max(parseInt(position, 10), 0) * 1000,
+        }
+        const device_id = await getState('device_id')
+        const endpoint = `/play?device_id=${device_id}`
+        return await makeRequest({ endpoint, type: 'player', params: { method: 'PUT', body } })
     })
 }
 
 
 function seekTrackToPosition({ position }) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const options = await setOptions({ method: 'PUT' })
-            const device_id = await getState('device_id')
-            const url = `${API_URL}/seek?position_ms=${position}&device_id=${device_id}`
-            const response = await request({ url, options })
-            resolve(response)
-        } catch (error) { reject(error) }
+    return handleRequest(async () => {
+        const device_id = await getState('device_id')
+        const endpoint = `/seek?position_ms=${position}&device_id=${device_id}`
+        return await makeRequest({ endpoint, type: 'player', params: { method: 'PUT' } })
     })
 }
+
 export { playSharedTrack, seekTrackToPosition }
