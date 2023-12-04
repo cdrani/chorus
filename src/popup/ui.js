dddffafae3c72c9aaa2a4cb5b5a383e86ded6e0b
cdrani <charlesdrani@gmail.com>
2023-12-04 00:00:28 -0700

    feat: add popup ui for media controls

--- src/popup/ui.js
+++ src/popup/ui.js
@@ -12,6 +12,48 @@ const trackText = id => `
     </div>
 `
 
+const SVG_PATHS = {
+    seek: '<path d="M34.46,53.91A21.91,21.91,0,1,0,12.55,31.78"/><polyline points="4.65 22.33 12.52 32.62 22.81 24.75"/>',
+    block: '<path fill-rule="evenodd" d="M5.965 4.904l9.131 9.131a6.5 6.5 0 00-9.131-9.131zm8.07 10.192L4.904 5.965a6.5 6.5 0 009.131 9.131zM4.343 4.343a8 8 0 1111.314 11.314A8 8 0 014.343 4.343z" clip-rule="evenodd"/>',
+    play: '<path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"/>',
+    next: '<path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-1.6z"/>',
+    previous: '<path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7h1.6z"/>',
+    heart: '<path d="M15.724 4.22A4.313 4.313 0 0 0 12.192.814a4.269 4.269 0 0 0-3.622 1.13.837.837 0 0 1-1.14 0 4.272 4.272 0 0 0-6.21 5.855l5.916 7.05a1.128 1.128 0 0 0 1.727 0l5.916-7.05a4.228 4.228 0 0 0 .945-3.577z"/>',
+    shuffle: '<path d="M13.151.922a.75.75 0 1 0-1.06 1.06L13.109 3H11.16a3.75 3.75 0 0 0-2.873 1.34l-6.173 7.356A2.25 2.25 0 0 1 .39 12.5H0V14h.391a3.75 3.75 0 0 0 2.873-1.34l6.173-7.356a2.25 2.25 0 0 1 1.724-.804h1.947l-1.017 1.018a.75.75 0 0 0 1.06 1.06L15.98 3.75 13.15.922zM.391 3.5H0V2h.391c1.109 0 2.16.49 2.873 1.34L4.89 5.277l-.979 1.167-1.796-2.14A2.25 2.25 0 0 0 .39 3.5z"/><path d="m7.5 10.723.98-1.167.957 1.14a2.25 2.25 0 0 0 1.724.804h1.947l-1.017-1.018a.75.75 0 1 1 1.06-1.06l2.829 2.828-2.829 2.828a.75.75 0 1 1-1.06-1.06L13.109 13H11.16a3.75 3.75 0 0 1-2.873-1.34l-.787-.938z"/>'
+}
+
+const createIcon = ({ type, id = '', role = '', lw = 20, viewBox = '-4 -4 24 24', styles = '' }) => `
+    <button 
+        role="${role || type}"
+        id="${(id || type) + '-btn'}"
+        style="padding:0;border:none;background:none;display:flex;justify-content:center;align-items:center;cursor:pointer;${styles}"
+    > 
+        <svg
+            width="${lw}"
+            height="${lw}"
+            stroke-width="1.5"
+            viewBox="${viewBox}"
+            stroke="currentColor"
+            id="${(id || type) + '-icon'}"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid meet"
+            fill="${type == 'seek' ? 'none' : 'currentColor'}"
+            style="${id == 'ff' ? 'transform: scaleX(-1)' : ''}"
+        >
+            ${SVG_PATHS[type]}
+        </svg>
+    </button>
+`
+
+const generateMediaIcons = () => `
+    ${createIcon({ type: 'shuffle' })}
+    ${createIcon({ type: 'seek', role: 'seek-rewind', id: 'rw', viewBox: '0 0 64 64' })}
+    ${createIcon({ type: 'previous', lw: 24 })}
+    ${createIcon({ type: 'play', role: 'play/pause', styles: 'border-radius:32px;width:32px;height:32px' })}
+    ${createIcon({ type: 'next', lw: 24 })}
+    ${createIcon({ type: 'seek', role: 'seek-fastforward', id: 'ff', viewBox: '0 0 64 64' })}
+`
+
 export const createRootContainer = () => `
     <div id="chorus">
         <div id="chorus-toggle">${extToggle.ui}</div>
@@ -20,6 +62,18 @@ export const createRootContainer = () => `
             <div style="display:flex;flex-direction:column;width:100%;justify-content:space-evenly">
                 ${trackText('track-title')}
                 ${trackText('track-artists')}
+            </div>
+        </div>
+
+        <div id="controls" style="display:flex;width:100%;height:32px;align-items:center;justify-content:space-between">
+            <div id="left-controls" style="display:flex;justify-content:space-between;width:40px;margin-right:20px;">
+                ${createIcon({ type: 'heart', role: 'save/unsave', viewBox: '-6 -6 24 24' })}
+                ${createIcon({ type: 'block', role: 'block-track' })}
+            </div>
+            <div id="right-controls" style="display:flex;width:100%;justify-content:center;align-items:center">
+                <div style="display:flex;width:100%;justify-content:space-around;align-items:center">
+                    ${generateMediaIcons()}
+                </div>
             </div>
         </div>
     </div>
