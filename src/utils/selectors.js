9b05536eb16bda551aab457d9ebcff2f23f68b89
cdrani <charlesdrani@gmail.com>
2023-12-07 16:15:49 -0700

    refactor: extract command selector objects to util

---
+++ src/utils/selectors.js
@@ -0,0 +1,18 @@
+const mediaKeys = {
+    'repeat': '[data-testid="control-button-repeat"]',
+    'shuffle': '[data-testid="control-button-shuffle"]',
+    'next': '[data-testid="control-button-skip-forward"]',
+    'previous': '[data-testid="control-button-skip-back"]',
+    'play/pause': '[data-testid="control-button-playpause"]',
+    'mute/unmute': '[data-testid="volume-bar-toggle-mute-button"]',
+    'save/unsave': '[data-testid="now-playing-widget"] > [data-testid="add-button"]',
+}
+
+const chorusKeys = {
+    'settings': '#chorus-icon',
+    'block-track': '#chorus-skip',
+    'seek-rewind': '#seek-player-rw-button',
+    'seek-fastforward': '#seek-player-ff-button',
+}
+
+export { mediaKeys, chorusKeys }
