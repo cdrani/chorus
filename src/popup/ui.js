7562a7ea00224355818aab52f1567424e017eaa1
cdrani <charlesdrani@gmail.com>
2023-12-07 15:35:03 -0700

    feat: add shuffle & repeat dot ui

--- src/popup/ui.js
+++ src/popup/ui.js
@@ -12,10 +12,11 @@ const trackText = id => `
     </div>
 `
 
-const SVG_PATHS = {
+export const SVG_PATHS = {
     seek: '<path d="M34.46,53.91A21.91,21.91,0,1,0,12.55,31.78"/><polyline points="4.65 22.33 12.52 32.62 22.81 24.75"/>',
     block: '<path fill-rule="evenodd" d="M5.965 4.904l9.131 9.131a6.5 6.5 0 00-9.131-9.131zm8.07 10.192L4.904 5.965a6.5 6.5 0 009.131 9.131zM4.343 4.343a8 8 0 1111.314 11.314A8 8 0 014.343 4.343z" clip-rule="evenodd"/>',
     play: '<path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"/>',
+    pause: '<path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"/>',
     next: '<path d="M12.7 1a.7.7 0 0 0-.7.7v5.15L2.05 1.107A.7.7 0 0 0 1 1.712v12.575a.7.7 0 0 0 1.05.607L12 9.149V14.3a.7.7 0 0 0 .7.7h1.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-1.6z"/>',
     previous: '<path d="M3.3 1a.7.7 0 0 1 .7.7v5.15l9.95-5.744a.7.7 0 0 1 1.05.606v12.575a.7.7 0 0 1-1.05.607L4 9.149V14.3a.7.7 0 0 1-.7.7H1.7a.7.7 0 0 1-.7-.7V1.7a.7.7 0 0 1 .7-.7h1.6z"/>',
     heart: '<path d="M15.724 4.22A4.313 4.313 0 0 0 12.192.814a4.269 4.269 0 0 0-3.622 1.13.837.837 0 0 1-1.14 0 4.272 4.272 0 0 0-6.21 5.855l5.916 7.05a1.128 1.128 0 0 0 1.727 0l5.916-7.05a4.228 4.228 0 0 0 .945-3.577z"/>',
@@ -24,12 +25,18 @@ const SVG_PATHS = {
     repeat1: '<path d="M0 4.75A3.75 3.75 0 0 1 3.75 1h.75v1.5h-.75A2.25 2.25 0 0 0 1.5 4.75v5A2.25 2.25 0 0 0 3.75 12H5v1.5H3.75A3.75 3.75 0 0 1 0 9.75v-5zM12.25 2.5h-.75V1h.75A3.75 3.75 0 0 1 16 4.75v5a3.75 3.75 0 0 1-3.75 3.75H9.81l1.018 1.018a.75.75 0 1 1-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 1 1 1.06 1.06L9.811 12h2.439a2.25 2.25 0 0 0 2.25-2.25v-5a2.25 2.25 0 0 0-2.25-2.25z"/><path d="M9.12 8V1H7.787c-.128.72-.76 1.293-1.787 1.313V3.36h1.57V8h1.55z"/>'
 }
 
+const seekPosition = { ff: 'left:50%', rw: 'right:50%' }
+
 const createIcon = ({ type, id = '', role = '', lw = 20, viewBox = '-4 -4 24 24', styles = '' }) => `
     <button 
         role="${role || type}"
         id="${(id || type) + '-btn'}"
-        style="padding:0;border:none;background:none;display:flex;justify-content:center;align-items:center;cursor:pointer;${styles}"
+        style="position:relative;padding:0;border:none;background:none;display:flex;justify-content:center;align-items:center;cursor:pointer;${styles}"
     > 
+        ${id !== '' ? `<span id="seek-${id}" style="position:absolute;z-index:-10;background:transparent;font-weight:bold;font-size:small;top:46%;text-align:center;${seekPosition[id]}">10</span>` : ''}
+        ${['repeat', 'shuffle'].includes(type) 
+            ? `<span style="display:none;position:absolute;height:0;bottom:6px" id="${type}-dot">&bull;</span>` : ''
+        }
         <svg
             width="${lw}"
             height="${lw}"
@@ -67,13 +74,13 @@ export const createRootContainer = () => `
             </div>
         </div>
 
-        <div id="controls" style="display:flex;width:100%;height:32px;align-items:center;justify-content:space-between">
+        <div id="media-controls" style="display:flex;width:100%;height:32px;align-items:center;justify-content:space-between">
             <div id="left-controls" style="display:flex;justify-content:space-between;width:40px;margin-right:20px;">
-                ${createIcon({ type: 'heart', role: 'save/unsave', viewBox: '-6 -6 24 24' })}
-                ${createIcon({ type: 'block', role: 'block-track' })}
+                ${createIcon({ type: 'heart', role: 'save/unsave', lw: 22, viewBox: '-6 -6 24 24' })}
+                ${createIcon({ type: 'block', role: 'block-track', lw: 22 })}
             </div>
             <div id="right-controls" style="display:flex;width:100%;justify-content:center;align-items:center">
-                <div style="display:flex;width:100%;justify-content:space-around;align-items:center">
+                <div style="display:flex;width:100%;justify-content:space-between;align-items:center">
                     ${generateMediaIcons()}
                 </div>
             </div>
