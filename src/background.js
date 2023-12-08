11613f37a2f092e32ebe5c174cc055cd3970f40c
cdrani <charlesdrani@gmail.com>
2023-12-08 13:36:30 -0700

    feat: send ui-state between background & popup

--- src/background.js
+++ src/background.js
@@ -1,5 +1,6 @@
 import { setState, getState } from './utils/state.js'
-import { getActiveTab, sendMessage } from './utils/messaging.js'
+import { mediaKeys, chorusKeys } from './utils/selectors.js'
+import { activeOpenTab, sendMessage } from './utils/messaging.js'
 
 import { createArtistDiscoPlaylist } from './services/artist-disco.js'
 import { playSharedTrack, seekTrackToPosition } from './services/player.js'
@@ -7,14 +8,56 @@ import { playSharedTrack, seekTrackToPosition } from './services/player.js'
 let ENABLED = true
 let popupPort = null
 
-chrome.runtime.onConnect.addListener(port => {
+async function getUIState({ selector, tabId }) {
+    const result = await chrome.scripting.executeScript({
+        args: [selector],
+        target: { tabId },
+        func: selector => document.querySelector(selector)?.getAttribute('aria-label').toLowerCase()
+    })
+
+    return result?.at(0).result
+}
+
+async function getMediaControlsState(tabId) {
+    const requiredKeys = ['repeat', 'shuffle', 'play/pause', 'save/unsave']
+    const selectors = Object.keys(mediaKeys).map(key => (
+        requiredKeys.includes(key) ? mediaKeys[key] : undefined
+    )).filter(Boolean)
+
+    const promises = selectors.map(selector => (
+        new Promise(resolve => {
+            if (!selector.includes('add-button')) return resolve(getUIState({ selector, tabId }))
+            return setTimeout(() => resolve(getUIState({ selector, tabId })), 300)
+        })
+    ))
+
+    const results = await Promise.allSettled(promises)
+    const state = results.map((item, idx) => ({ data: item.value, key: requiredKeys[idx] }))
+    return state
+}
+
+async function setMediaState({ active, tabId }) {
+    popupPort.postMessage({ type: 'ui-state', data: { active } })
+
+    if (!active) return
+
+    const state = await getMediaControlsState(tabId)
+    return popupPort.postMessage({ type: 'state', data: state })
+}
+
+chrome.runtime.onConnect.addListener(async port => {
     if (port.name !== 'popup') return
 
     popupPort = port
+    const { active, tabId } = await activeOpenTab()
+    await setMediaState({ active, tabId })
+
     port.onMessage.addListener(async message => {
         if (message?.type !== 'controls') return
 
-        await executeButtonClick({ command: message.key })
+        const { selector, tabId } = await executeButtonClick({ command: message.key })
+        const result = await getUIState({ selector, tabId })
+        port.postMessage({ type: 'controls', data: { key: message.key, result } })
     })
 
     port.onDisconnect.addListener(() => (popupPort = null))
@@ -39,7 +82,7 @@ function updateBadgeState({ changes, changedKey }) {
     setBadgeInfo(ENABLED)
 }
 
-chrome.storage.onChanged.addListener(changes => {
+chrome.storage.onChanged.addListener(async changes => {
     const keys = Object.keys(changes)
     const changedKey = keys.find(key => (
         ['now-playing', 'enabled', 'auth_token', 'device_id'].includes(key)
@@ -50,7 +93,11 @@ chrome.storage.onChanged.addListener(changes => {
     updateBadgeState({ changes, changedKey })
 
     if (changedKey == 'now-playing' && ENABLED) {
-        return popupPort?.postMessage({ type: changedKey, data: changes[changedKey].newValue }) 
+        if (!popupPort) return
+
+        const { active, tabId } = await activeOpenTab()
+        active && popupPort.postMessage({ type: changedKey, data: changes[changedKey].newValue }) 
+        return await setMediaState({ active, tabId })
     }
 
     const messageValue = changedKey == 'enabled' ? changes[changedKey] : changes[changedKey].newValue
@@ -61,7 +108,6 @@ chrome.webRequest.onBeforeRequest.addListener(details => {
     const rawBody = details?.requestBody?.raw?.at(0)?.bytes
     if (!rawBody) return
 
-    // Decoding the ArrayBuffer to a UTF-8 string
     const text = new TextDecoder('utf-8').decode(new Uint8Array(rawBody))
     const data = JSON.parse(text)
     chrome.storage.local.set({ device_id: data.device.device_id.toString() })
@@ -103,43 +149,26 @@ chrome.runtime.onMessage.addListener(({ key, values }, _, sendResponse) => {
 })
 
 async function executeButtonClick({ command, isShortCutKey = false }) {
-    const tab = await getActiveTab()
-    if (!tab) return
+    const { active, tabId } = await activeOpenTab()
+    if (!active) return
 
     if (command == 'on/off') {
         const enabled = await getState('enabled')        
         return chrome.storage.local.set({ enabled: !enabled })
     } 
 
-    const chorusMap = {
-        'settings': '#chorus-icon',
-        'block-track': '#chorus-skip',
-        'seek-rewind': '#seek-player-rw-button',
-        'seek-fastforward': '#seek-player-ff-button',
-    }
-
-    const mediaMap = {
-        'repeat': '[data-testid="control-button-repeat"]',
-        'shuffle': '[data-testid="control-button-shuffle"]',
-        'next': '[data-testid="control-button-skip-forward"]',
-        'previous': '[data-testid="control-button-skip-back"]',
-        'play/pause': '[data-testid="control-button-playpause"]',
-        'mute/unmute': '[data-testid="volume-bar-toggle-mute-button"]',
-        'save/unsave': '[data-testid="now-playing-widget"] > [data-testid="add-button"]',
-    }
-
-    const selector = chorusMap[command] || mediaMap[command]
-    const isChorusCommand = Object.keys(chorusMap).includes(command)
+    const selector = chorusKeys[command] || mediaKeys[command]
+    const isChorusCommand = Object.keys(chorusKeys).includes(command)
 
     if (isShortCutKey && !ENABLED && isChorusCommand) return
 
     await chrome.scripting.executeScript({
         args: [selector],
-        target: { tabId: tab.id },
+        target: { tabId },
         func: selector => document.querySelector(selector)?.click(),
     })
+
+    if (!isShortCutKey) return { selector, tabId }
 }
 
-chrome.commands.onCommand.addListener(async command => {
-    await executeButtonClick({ command, isShortCutKey: true })
-})
+chrome.commands.onCommand.addListener(async command => await executeButtonClick({ command, isShortCutKey: true }))
