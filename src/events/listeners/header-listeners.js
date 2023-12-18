a28385803f6cb293fc29e3e8d7b9b3d10d271691
cdrani <charlesdrani@gmail.com>
2023-12-17 18:22:06 -0700

    fix: snip controls inputs not reset on view enter

--- src/events/listeners/header-listeners.js
+++ src/events/listeners/header-listeners.js
@@ -1,4 +1,5 @@
 import Listeners from './listeners.js'
+import { spotifyVideo } from '../../actions/overload.js'
 
 export default class HeaderListeners extends Listeners {
     constructor(songTracker) {
@@ -6,6 +7,7 @@ export default class HeaderListeners extends Listeners {
 
         this._setup = false
         this._viewInFocus = null
+        this._video = spotifyVideo.element
         this._VIEWS = ['snip', 'speed', 'fx', 'seek']
     }
 
@@ -18,7 +20,7 @@ export default class HeaderListeners extends Listeners {
         this.#effectsViewToggle()
         this.#closeModalListener()
 
-        this._currentView = 'snip'
+        this.currentView = 'snip'
         this._setup = true
     }
 
@@ -30,13 +32,15 @@ export default class HeaderListeners extends Listeners {
     #seekViewToggle() {
         const seekButton = document.getElementById('chorus-seek-button')
         seekButton?.addEventListener('click', async () => {
-            this._currentView = 'seek'
+            this.currentView = 'seek'
             await this._seek.init()
         })
     }
 
-    set _currentView(selectedView) {
+    set currentView(selectedView = 'snip') {
         this._viewInFocus = selectedView
+        if (selectedView != 'snip') this._video.resetTempTimes()
+
         this._VIEWS.forEach(view => {
             const viewButton = document.getElementById(`chorus-${view}-button`)
             const viewInFocusContainer = document.getElementById(`chorus-${view}-controls`)
@@ -49,13 +53,16 @@ export default class HeaderListeners extends Listeners {
 
     #snipViewToggle() {
         const snipButton = document.getElementById('chorus-snip-button')
-        snipButton?.addEventListener('click', () => { this._currentView = 'snip' })
+        snipButton?.addEventListener('click', async () => { 
+              this.currentView = 'snip'
+              await this._snip.init()
+        })
     }
 
     #speedViewToggle() {
         const speedButton = document.getElementById('chorus-speed-button')
         speedButton?.addEventListener('click', async () => {
-            this._currentView = 'speed'
+            this.currentView = 'speed'
             await this._speed.init()
         })
     }
@@ -63,7 +70,7 @@ export default class HeaderListeners extends Listeners {
     #effectsViewToggle() {
         const effectsButton = document.getElementById('chorus-fx-button')
         effectsButton?.addEventListener('click', () => {
-            this._currentView = 'fx'
+            this.currentView = 'fx'
             this._reverb.init()
         })
     }
