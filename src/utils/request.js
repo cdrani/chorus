e295e782ee97ec11a29e965acc8f9e44496ed63d
cdrani <charlesdrani@gmail.com>
2024-05-15 08:09:30 -0600

    feat: add optional connection_id to request header

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,18 +1,23 @@
 import { getState } from './state.js'
 
-export const setOptions = async ({ method = 'GET', body = null }) => {
+export const setOptions = async ({ method = 'GET', body = null, connect = false  }) => {
     const authHeader = await getAuthHeader()
+    const connectHeader = connect ? await getConnectHeader() : null
+
     return {
         method,
         body: body ? JSON.stringify(body) : null,
-        headers: { Authorization: authHeader, 'Content-Type': 'application/json' },
+        headers: { 
+            Authorization: authHeader, 
+            'Content-Type': 'application/json',
+            ...connect && { 'X-Spotify-Connection-Id': connectHeader },
+        },
     }
 }
 
-const getAuthHeader = async () => {
-    const authHeader = await getState('auth_token')
-    return authHeader
-}
+const getAuthHeader = async () => await getState('auth_token')
+
+const getConnectHeader = async () => await getState('connection_id')
 
 export const request = async ({ url, options }) => {
     try {
