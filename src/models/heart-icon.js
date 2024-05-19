6d56ecffdef9274a82c76a27701699eabe73f9e3
cdrani <charlesdrani@gmail.com>
2024-05-19 16:38:30 -0600

    feat: connect heart icon to service + sync state

--- src/models/heart-icon.js
+++ src/models/heart-icon.js
@@ -1,25 +1,36 @@
 import { parseNodeString } from '../utils/parser.js'
 import { highlightIconTimer } from '../utils/highlight.js'
 
+import Dispatcher from '../events/dispatcher.js'
+import { currentData } from '../data/current.js'
 import { createIcon, HEART_ICON } from '../components/icons/icon.js'
 
 export default class HeartIcon {
     constructor(type = 'current') {
         this._type = type
+        this._dispatcher = new Dispatcher()
     }
 
     init() { this.#placeIcon(); this.#setupListener(); }
 
     removeIcon() {
         this.#heartIcon?.remove()
+        this.#toggleNowPlayingButton(true)
     }
 
     #placeIcon() {
-        const refNode = document.querySelector('[data-testid="now-playing-widget"] > button')
         const heartButton = parseNodeString(this.#createHeartIcon)
+        const refNode = this.#nowPlayingButton
         refNode.parentElement.insertBefore(heartButton, refNode)
+        this.#toggleNowPlayingButton(false)
     }
 
+    #toggleNowPlayingButton(show) {
+        const button = this.#nowPlayingButton
+        button.style.visibility = show ? 'visible' : 'hidden'
+        button.style.padding = show ? '0.5rem' : 0
+        button.style.width = show ? '1rem' : 0
+    }
 
     get #createHeartIcon() {
         return createIcon(HEART_ICON)
@@ -30,34 +41,56 @@ export default class HeartIcon {
     }
 
     #setupListener() {
-        this.#heartIcon?.addEventListener('click', () => {})
+        this.#heartIcon?.addEventListener('click', async () => this.#handleClick())
+    }
+
+    async #dispatchLikedTracks() {
+        const method = this.#isHeartIconHighlighted ? 'DELETE' : 'PUT'
+        const { trackId: id } = await currentData.readTrack()
+
+        return await this._dispatcher.sendEvent({
+            eventType: 'tracks.update',
+            detail: { key: 'tracks.update', values: { id, method } },
+        })
+    }
+
+    async #handleClick() {
+        await this.#dispatchLikedTracks()
+        this.highlightIcon(!this.#isHeartIconHighlighted)
     }
 
     get isCurrent() {
         return this._type == 'current'
     }
 
-    get icon() {
-        const selector = this.isCurrent ? '#chorus-heart' : ''
-        return document.querySelector(selector)
+    get #nowPlayingButton() {
+        return document.querySelector('div[data-testid="now-playing-widget"] > button[data-encore-id="buttonTertiary"]')
     }
 
-    get nowPlayingButton() {
-        return document.querySelector('div[data-testid="now-playing-widget"] > button')
-    }
-
-    get isHighlighted() {
-        const button = this.nowPlayingButton
+    get #isSpotifyHighlighted() {
+        const button = this.#nowPlayingButton
         if (!button) return false
+
         return JSON.parse(button.getAttribute('aria-checked'))
     }
 
-    highlightIcon() {
+    get #isHeartIconHighlighted() {
+        return this.#heartIcon.firstElementChild.getAttribute('fill') != 'unset'
+    }
+
+    highlightIcon(highlight) {
+        const update = highlight ?? this.#isSpotifyHighlighted
         highlightIconTimer({ 
             fill: true,
-            highlight: this.isHighlighted,
+            highlight: update,
             selector: '#chorus-heart > svg',
         })
+
+        this.#updateIconLabel(update)
     }
-     
+
+    #updateIconLabel(highlight) {
+        const text = `${highlight ? 'Remove from' : 'Save to' } Liked`
+        this.#heartIcon.setAttribute('aria-label', text)
+    }
 }
