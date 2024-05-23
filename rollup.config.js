fc940df812a2efe0bd0d321461a9f9b3c65d8dab
Stephany <128330097+sstephanyy@users.noreply.github.com>
2024-05-22 21:40:25 -0300

    fix: chunk file generation for Windows: ensure [name]-[hash].js naming (#218)

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
+import path from 'path'
 import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
 
 export default {
     input: 'src/manifest.chrome.json',
     output: {
         dir: 'dist',
-        format: 'esm'
+        format: 'esm',
+        chunkFileNames: path.join('chunks', '[name]-[hash].js')
+
         // sourcemap: true
     },
     plugins: [chromeExtension(), simpleReloader()]
