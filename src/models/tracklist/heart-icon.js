20748072dd3e3a561d417f45d682f219b2657950
cdrani <charlesdrani@gmail.com>
2024-05-24 21:08:30 -0600

    feat: heart icon like/unlike in tracklist

--- src/models/tracklist/heart-icon.js
+++ src/models/tracklist/heart-icon.js
@@ -1,6 +1,11 @@
 import TrackListIcon from './tracklist-icon.js'
 import { TRACK_HEART, createIcon } from '../../components/icons/icon.js'
 
+import { getTrackId } from '../../utils/song.js'
+import Dispatcher from '../../events/dispatcher.js'
+import { currentData } from '../../data/current.js'
+import { highlightIconTimer } from '../../utils/highlight.js'
+
 export default class HeartIcon extends TrackListIcon {
     constructor(store) {
         super({
@@ -8,11 +13,63 @@ export default class HeartIcon extends TrackListIcon {
             key: 'isLiked',
             selector: 'button[role="heart"]'
         })
+        this._dispatcher = new Dispatcher()
+    }
+
+    get #isOnLikedSongsPage() {
+        return location.pathname == '/collection/tracks'
     }
 
     #isLiked(row) {
-        const button = row.querySelector('button[data-encore-id="buttonTertiary"]')
-        return JSON.parse(button.getAttribute('aria-checked'))
+        if (this.#isOnLikedSongsPage) return true
+
+        if (row.getAttribute('data-testid') != 'tracklist-row') return false
+
+        const { trackId = null } = getTrackId(row)
+        if (!trackId) return false
+
+        return !!this._store.checkInCollection(trackId)
+    }
+
+    async toggleTrackLiked(row) {
+        const { trackId = null } = getTrackId(row)
+        if (!trackId) return
+
+        const isLiked = this.#isOnLikedSongsPage ? true : this.#isLiked(row)
+        const method = isLiked ? 'DELETE' : 'PUT'
+
+        const { state } = await this._dispatcher.sendEvent({
+            eventType: 'tracks.update',
+            detail: { key: 'tracks.update', values: { id: trackId, method } }
+        })
+
+        if (state == 'error') return
+
+        const saved = method == 'PUT'
+        this._store.saveInCollection({ id: trackId, saved })
+
+        const icon = row.querySelector(this._selector)
+        this.animate(icon, saved)
+
+        this.#updateCurrentTrack(trackId, saved)
+    }
+
+    async #updateCurrentTrack(trackId, highlight) {
+        const currentTrack = await currentData.readTrack()
+
+        if (!currentTrack?.trackId) return
+
+        if (currentTrack?.trackId !== trackId) return
+
+        highlightIconTimer({
+            highlight,
+            fill: true,
+            selector: '#chorus-heart > svg'
+        })
+
+        const heartIcon = document.querySelector('#chorus-heart')
+        const text = `${highlight ? 'Remove from' : 'Save to'} Liked`
+        heartIcon?.setAttribute('aria-label', text)
     }
 
     setInitialState(row) {
@@ -50,9 +107,11 @@ export default class HeartIcon extends TrackListIcon {
         })
     }
 
-    animate(icon) {
+    animate(icon, heart) {
         if (!icon?.parentElement) return
-        const isLiked = this.#isLiked(icon.parentElement)
+
+        const isLiked = heart ?? this.#isLiked(icon?.parentElement?.parentElement)
+
         this.#burn({ icon, burn: isLiked })
         this.#glow({ icon, glow: isLiked })
     }
