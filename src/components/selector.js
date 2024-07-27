7c1558a072b26ff68fbf4b9bd5a6e02d17ef6245
cdrani <charlesdrani@gmail.com>
2024-07-27 16:57:18 -0600

    styles: allow selector width to stretch

--- src/components/selector.js
+++ src/components/selector.js
@@ -1,5 +1,5 @@
 const btnStyles =
-    'text-align:end;cursor:pointer;background:#171717;padding:2px;padding-right:16px;color:#fff;font-size:1rem;'
+    'text-align:end;cursor:pointer;background:#171717;padding:2px;padding-right:12px;color:#fff;font-size:1rem;'
 
 const createOptions = (optionNames) =>
     optionNames
@@ -13,7 +13,7 @@ export const createSelector = ({ labelName, name, optionNames }) => `
         <label style="color:#b3b3b3;font-size:">${labelName}</label>
 
         <form id="select-container" class="selector" style="overflow-y:auto;max-height:100px;">
-            <button id="${name}-btn" style="display:flex;align-content:center;border:solid 1px #fff;background:transparent;color:#b3b3b3;width:100px;justify-content:end;">
+            <button id="${name}-btn" style="display:flex;align-content:center;border:solid 1px #fff;background:transparent;color:#b3b3b3;min-width:100px;justify-content:end;">
                 <span id="${name}-selected" style="color:#fff;font-size:1rem;margin-right: 4px">none</span>
                 <svg style="width:1rem;height:1rem;margin-top:4px;" viewBox="0 0 256 256" fill="#fff" xmlns="http://www.w3.org/2000/svg">
                     <path
@@ -24,7 +24,7 @@ export const createSelector = ({ labelName, name, optionNames }) => `
 
             <div
                 id="${name}-list"
-                style="min-width:100px;border:1px solid #b3b3b3;border-radius:4px;right:0;overflow-y:auto;background:#171717;flex-direction:column;max-height:110px;z-index:100;position:absolute;display:none"
+                style="margin-top:4px;min-width:120px;border:1px solid #b3b3b3;border-radius:4px;right:0;overflow-y:auto;background:#171717;flex-direction:column;max-height:110px;z-index:100;position:absolute;display:none"
             >
                 ${createOptions(['none', ...optionNames])}
             </div>
