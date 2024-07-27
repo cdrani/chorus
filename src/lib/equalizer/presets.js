c0284ee33c0daddd38922e6a1f8149216c46f5bd
cdrani <charlesdrani@gmail.com>
2024-07-27 16:59:50 -0600

    feat: add more EQ presets & filters

--- src/lib/equalizer/presets.js
+++ src/lib/equalizer/presets.js
@@ -1,13 +1,19 @@
 export const EQ_FILTERS = [
-    { freq: 64, gain: 0, type: 'peaking' },
+    { freq: 20, gain: 0, type: 'lowshelf' }, // Sub-bass frequencies
     { freq: 32, gain: 0, type: 'lowshelf' },
+    { freq: 50, gain: 0, type: 'lowshelf' }, // Bass boost
+    { freq: 64, gain: 0, type: 'peaking' },
     { freq: 125, gain: 0, type: 'peaking' },
     { freq: 250, gain: 0, type: 'peaking' },
     { freq: 500, gain: 0, type: 'peaking' },
     { freq: 1000, gain: 0, type: 'peaking' },
     { freq: 2000, gain: 0, type: 'peaking' },
+    { freq: 3000, gain: 0, type: 'peaking' }, // Presence boost
     { freq: 4000, gain: 0, type: 'peaking' },
+    { freq: 6000, gain: 0, type: 'peaking' }, // Presence boost
     { freq: 8000, gain: 0, type: 'peaking' },
+    { freq: 12000, gain: 0, type: 'peaking' }, // Air boost
+    { freq: 14000, gain: 0, type: 'highshelf' }, // Treble cut
     { freq: 16000, gain: 0, type: 'highshelf' }
 ]
 
@@ -29,8 +35,19 @@ export const EQ_PRESETS = {
     LargeHall: [6.24, 6.24, 3.36, 3.36, 0.0, -2.88, -2.88, -2.88, 0.0, 0.0],
     Bass: [4.8, 5.76, 5.76, 3.36, 0.96, -2.4, -4.8, -6.24, -6.72, -6.72],
     Treble: [-5.76, -5.76, -5.76, -2.4, 1.44, 6.72, 9.6, 9.6, 9.6, 10.08],
-    LaptopSpeakers: [2.88, 6.72, 3.36, -1.92, -1.44, 0.96, 2.88, 5.76, 7.68, 8.64],
-    BassAndTreble: [4.32, 3.36, 0.0, -4.32, -2.88, 0.96, 4.8, 6.72, 7.2, 7.2]
+    Laptop: [2.88, 6.72, 3.36, -1.92, -1.44, 0.96, 2.88, 5.76, 7.68, 8.64],
+    BassTreble: [4.32, 3.36, 0.0, -4.32, -2.88, 0.96, 4.8, 6.72, 7.2, 7.2],
+    VocalBoost: [0.0, 0.0, 2.4, 4.8, 4.8, 2.4, 0.0, -1.2, -1.2, 0.0],
+    Acoustic: [1.2, 1.2, 0.0, -1.2, 0.0, 1.2, 2.4, 2.4, 1.2, 0.0],
+    Podcast: [0.0, 0.0, 1.2, 2.4, 4.8, 2.4, 0.0, -1.2, -2.4, -3.6],
+    BassBoostPlus: [6.0, 6.0, 6.0, 3.6, 0.0, -3.0, -6.0, -7.2, -7.2, -7.2],
+    TrebleBoost: [-6.0, -6.0, -6.0, -3.0, 0.0, 3.0, 6.0, 7.2, 7.2, 8.4],
+    SmallRoom: [2.0, 1.5, 0.0, -1.5, -2.0, -1.5, 0.0, 1.5, 2.0, 2.5],
+    MediumRoom: [1.5, 1.0, 0.0, -1.0, -1.5, -1.0, 0.0, 1.0, 1.5, 2.0],
+    LargeRoom: [3.0, 2.0, 0.0, -2.0, -3.0, -2.0, 0.0, 2.0, 3.0, 4.0],
+    ConcertHall: [2.5, 2.0, 1.0, 0.0, -1.0, -1.0, 0.0, 2.0, 3.0, 3.5],
+    Auditorium: [2.0, 1.5, 0.0, -1.5, -2.0, -1.5, 0.5, 2.0, 2.5, 3.0],
+    Outdoor: [3.0, 2.5, 1.5, 0.0, -1.5, -1.5, 0.0, 2.0, 2.5, 3.0]
 }
 
 export const eqPresetLabels = Object.keys(EQ_PRESETS)
