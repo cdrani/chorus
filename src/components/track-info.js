e52b316cedc911638a873807365ca355395a82d5
cdrani <charlesdrani@gmail.com>
2024-07-28 21:48:08 -0600

    styles: adjust track-info width to new ui width

--- src/components/track-info.js
+++ src/components/track-info.js
@@ -1,6 +1,6 @@
 export const createTrackInfo = () => `
     <div style="display:flex;flex-direction:column;width:100%;justify-content:space-evenly;margin:.5rem 0">
-        <div class="container" style="width:282px"><div class="track-text" id="track-title"></div></div>
-        <div class="container" style="width:282px"><div class="track-text" id="track-artists"></div></div>
+        <div class="container" style="width:312px"><div class="track-text" id="track-title"></div></div>
+        <div class="container" style="width:312px"><div class="track-text" id="track-artists"></div></div>
     </div>
 `
